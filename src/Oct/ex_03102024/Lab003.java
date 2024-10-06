fa28cf62f7165ceaf965ed46594b638d91fbb80b
AMD <takalemanisha131@gmail.com>
2024-10-06 14:01:26 +0530

    New Exercise push

---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,18 @@
+package Oct.ex_03102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Table of 10");
+        System.out.println(10*1);
+        System.out.println(10*2);
+        System.out.println(10*3);
+        System.out.println(10*4);
+        System.out.println(10*5);
+        System.out.println(10*6);
+        System.out.println(10*7);
+        System.out.println(10*8);
+        System.out.println(10*9);
+        System.out.println(10*10);
+
+    }
+}
